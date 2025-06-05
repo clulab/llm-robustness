096985610c8cb903f2b1698a184f8298d3c5537e
dr.rivera2003@gmail.com <dr.rivera2003@gmail.com>
2025-06-04 21:00:36 -0700

    Json work
    just messing with json

--- print('Hello world').py
+++ print('Hello world').py
@@ -1,1 +1,22 @@
-print('Hello world')
+import json
+
+
+f = open('sentiment_analysis.json')
+
+# returns JSON object as a dictionary
+data = json.load(f)
+
+# Iterating through the json list
+print(data['persona'])
+print('\n')
+print(data['task'])
+print('\n')
+print(data['format'])
+print('\n')
+print(data['examples'])
+print('\n')
+print(data['query'])
+print('\n')
+
+# Closing file
+f.close()
