d622356a7954a7cba506653875335cfc742342bf
dr.rivera2003@gmail.com <dr.rivera2003@gmail.com>
2025-06-10 17:19:31 -0700

    Working with vllm

--- Main.py
+++ Main.py
@@ -1,18 +1,23 @@
 import json
 import copy
+import torch 
+from vllm import LLM, SamplingParams
+
+
+
+
+
 
 # Open file
-file = open('sentiment_analysis.json')
+#with open('sentiment_analysis.json') as file_object:
+
 
 # returns JSON object as a dictionary
-data = json.load(file)
+#data = json.load(file_object)
 
-#in case we want to make a copy of the json and change that copy
-# data_copy = copy.deepcopy(data)
-#data_copy['query'] = data_copy['query'].replace('<input>', 'Hello')
 
 # replace what the input is we want in the query
-data['query'] = data['query'].replace('<input>', 'Hello')
+#data['query'] = data['query'].replace('<input>', 'Hello')
 
 #copy printing
 #print(data_copy['query'])
@@ -21,8 +26,8 @@ data['query'] = data['query'].replace('<input>', 'Hello')
 #print('\n')
 
 # print the query data
-print(data['query'])
+#print(data['query'])
 
-# close json file
-file.close()
+#with open(filename) as file_object:
+#   i dont need parrelle 
 
