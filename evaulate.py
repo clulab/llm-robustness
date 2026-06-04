9631bd3ea5917dd55ab639d5167cf8d640c8a90f
danielrivera2 <danielrivera2@arizona.edu>
2026-06-04 12:28:52 -0700

    Update evaulate.py
    made the gold_answer go thru a .tolower()

--- evaulate.py
+++ evaulate.py
@@ -20,11 +20,16 @@ with open("output.json", "r") as file:
         if line:
             data.append(json.loads(line))
 
+
+
+
+
 y_true =[]
 y_pred =[]
 for item in data:
-    y_true.append(item['gold_answer'])
-    y_pred.append(item['output'].split()[0])  # Assuming the model's output is a string and we want the first word as the prediction
+    gold_answer = item['gold_answer'].lower()
+    y_true.append(gold_answer)
+    y_pred.append(item['output'].split()[0].lower())  # Assuming the model's output is a string and we want the first word as the prediction
 
 
 outputs =[]
@@ -39,21 +44,3 @@ with open("eval_output.json", "a", encoding="utf-8") as f:
     f.write("\n")  
 
 
-'''
-## to do
-Need to get jet stream up and running again 
-
-CSV 
-permutation model accuarcy dataset
-^^all the different titles
-
-
-
-
-
-we can remove p t and never remove the e and the f
-
-we can compare t vs e 
-
-we remove pieces from the best accuracy one
-'''
