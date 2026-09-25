7069939fc8c49d22aa8e133e579205d69c3ac279
danielrivera2 <danielrivera2@arizona.edu>
2026-09-25 11:47:41 -0700

    Update evaulate.py
    Fixed the output from cutting off words

--- evaulate.py
+++ evaulate.py
@@ -29,9 +29,8 @@ y_pred =[]
 for item in data:
     gold_answer = item['gold_answer'].lower()
     y_true.append(gold_answer)
-    y_pred.append(item['output'].split()[0].lower())  # Assuming the model's output is a string and we want the first word as the prediction
-
-
+    y_pred.append(item['output'].lower())  
+    
 outputs =[]
 accuracy = accuracy_score(y_true, y_pred)
 print(f"Accuracy: {accuracy:.3f}")
