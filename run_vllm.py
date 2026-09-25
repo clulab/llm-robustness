e3c015f8d7d1bd5345a5a0f6468b250ae2e908cf
danielrivera2 <danielrivera2@arizona.edu>
2026-09-25 12:10:02 -0700

    Update run_vllm.py

--- run_vllm.py
+++ run_vllm.py
@@ -73,34 +73,31 @@ def main():
                                 continue
                 #print(item['input'])
                 #print(combined_string.replace('<input>', item['input']))
-        output =model.generate(combined_string.replace('<input>', item['input']) , sampling_params =sampling_params)
+                output =model.generate(combined_string.replace('<input>', item['input']) , sampling_params =sampling_params)
 
         #This pulls out the start of the output from the llm model
-        outputs =output[0].outputs[0].text
+                outputs =output[0].outputs[0].text
 
-        if config['dataset'] == 'causal':
-                regex = r'No claim|Correlational|Causal'
-                regex_match = re.search(regex, outputs)
-                output_value = regex_match.group() if regex_match else outputs
+                if config['dataset'] == 'causal':
+                        regex = r'No claim|Correlational|Causal'
+                        regex_match = re.search(regex, outputs)
+                        output_value = regex_match.group() if regex_match else outputs
 
-        if config['dataset'] == 'sentiment_analysis':
-                regex = r'POSITIVE|NEGATIVE|NEUTRAL'
-                regex_match = re.search(regex, outputs)
-                output_value = regex_match.group() if regex_match else outputs
-        if config['dataset'] == 'negation':
-                regex = r'No|Yes'
-                regex_match = re.search(regex, outputs)
-                output_value = regex_match.group() if regex_match else outputs
-        print(f"Generated output for item {item['id']}: {output_value}")
-        new_outputs = {
+                if config['dataset'] == 'sentiment_analysis':
+                        regex = r'POSITIVE|NEGATIVE|NEUTRAL'
+                        regex_match = re.search(regex, outputs)
+                        output_value = regex_match.group() if regex_match else outputs
+                if config['dataset'] == 'negation':
+                        regex = r'No|Yes'
+                        regex_match = re.search(regex, outputs)
+                        output_value = regex_match.group() if regex_match else outputs
+                print(f"Generated output for item {item['id']}: {output_value}")
+                new_outputs = {
                 "id": item['id'],
                 "input": item['input'],
                 "gold_answer": item['gold_answer'],
                 "output": output_value,
-        }
-
-
-
+                }
         with open("output.json", "a", encoding="utf-8") as f:
                         json.dump(new_outputs, f)
                         f.write("\n")
